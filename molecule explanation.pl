c54c635647dd33ac04f7cc6cec14e56cc9c53bfb
Toph Wells <info@tophwells.com>
2015-12-22 17:33:47 +0100

    Renamed some variables and removed an unnecessary predicate from molecule_errors

--- molecule explanation.pl
+++ molecule explanation.pl
@@ -121,16 +121,16 @@ atom_name("uuo", uuo , ununoctium ).
 molecule(M,[An|T]):-atom_name(A,An,_),append(A,M2,M),molecule(M2,T).
 molecule([],[]).
 
-%molecule_errors(M,L,N): same as molecule, but N characters are not valid atoms. They are denoted by X.
-molecule_errors(M,ListOfAtoms,0):-molecule(M,ListOfAtoms).
-molecule_errors(M,[An|T],NumberOfErrors):- NumberOfErrors > 0, atom_name(A,An,_) ,append(A,M2,M), molecule_errors(M2,T,NumberOfErrors).
-molecule_errors([_|M],[x|T],NumberOfErrors):-NumberOfErrors>0, NewErrors is NumberOfErrors-1, molecule_errors(M,T,NewErrors).
+%molecule_some_errors(M,L,N): same as molecule, but N characters are not valid atoms. They are denoted by X.
+molecule_some_errors(M,ListOfAtoms,0):-molecule(M,ListOfAtoms).
+molecule_some_errors(M,[An|T],NumberOfErrors):- NumberOfErrors > 0, atom_name(A,An,_) ,append(A,M2,M), molecule_some_errors(M2,T,NumberOfErrors).
+molecule_some_errors([_|M],[x|T],NumberOfErrors):-NumberOfErrors>0, NewErrors is NumberOfErrors-1, molecule_some_errors(M,T,NewErrors).
 
-%molecule_min_errors(M,L,N): only if molecule_errors(M,L,N) and there are no solutions for smaller N
-molecule_min_errors(M,L,N):-molecule_min_max_errors(M,L,N,0).
+%molecule_errors(M,L): finds the smallest value of N for which molecule_some_errors(M,L,N) holds.
+molecule_errors(M,L):-molecule_min_max_errors(M,L,N,0).
 
 %molecule_min_max_errors(M,L,-N_correct,+N_attempt): helper function.
-molecule_min_max_errors(M,L,N_correct,N):-( molecule_errors(M,L,N) -> N_correct = N; N1 is N+1, molecule_min_max_errors(M,L,N_correct,N1)).
+molecule_min_max_errors(M,L,N_correct,N):-( molecule_some_errors(M,L,N) -> N_correct = N; N1 is N+1, molecule_min_max_errors(M,L,N_correct,N1)).
 
 %fullnames(Labb,Lname): Labb is a list of abbreviations of chemical elements and Lname is a list of the corresponding English names.
 fullnames([Habb|Tabb],[Hname|Tname]):-atom_name(_,Habb,Hname),fullnames(Tabb,Tname).
