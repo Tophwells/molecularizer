6ab93b55e435292bcb509838607ec6ccd0d0422f
Toph Wells <info@tophwells.com>
2015-12-22 16:41:05 +0100

    Reordering/spelling mistakes, added atom English names

--- molecule.pl
+++ molecule.pl
@@ -1,119 +1,123 @@
-atom_name("h",h).
-atom_name("he",he).
-atom_name("li",li).
-atom_name("be",be).
-atom_name("b",b).
-atom_name("c",c).
-atom_name("n",n).
-atom_name("o",o).
-atom_name("f",f).
-atom_name("ne",ne).
-atom_name("no",no).
-atom_name("al",al).
-atom_name("si",si).
-atom_name("p",p).
-atom_name("s",s).
-atom_name("na",na).
-atom_name("cl",cl).
-atom_name("ar",ar).
-atom_name("k",k).
-atom_name("ca",ca).
-atom_name("sc",sc).
-atom_name("ti",ti).
-atom_name("v",v).
-atom_name("cr",cr).
-atom_name("mn",mn).
-atom_name("fe",fe).
-atom_name("co",co).
-atom_name("ni",ni).
-atom_name("cu",cu).
-atom_name("zn",zn).
-atom_name("ga",ga).
-atom_name("de",de).
-atom_name("as",as).
-atom_name("se",se).
-atom_name("br",br).
-atom_name("kr",kr).
-atom_name("rb",rb).
-atom_name("sr",sr).
-atom_name("y",y).
-atom_name("zr",zr).
-atom_name("nb",nb).
-atom_name("mo",mo).
-atom_name("tc",tc).
-atom_name("ru",ru).
-atom_name("rh",rh).
-atom_name("pd",pd).
-atom_name("ag",ag).
-atom_name("cd",cd).
-atom_name("in",in).
-atom_name("sn",sn).
-atom_name("sb",sb).
-atom_name("te",te).
-atom_name("i",i).
-atom_name("xe",xe).
-atom_name("cs",cs).
-atom_name("ba",ba).
-atom_name("la",la).
-atom_name("ce",ce).
-atom_name("pr",pr).
-atom_name("nd",nd).
-atom_name("pm",pm).
-atom_name("sm",sm).
-atom_name("eu",eu).
-atom_name("gd",gd).
-atom_name("tb",tb).
-atom_name("dy",dy).
-atom_name("ho",ho).
-atom_name("er",er).
-atom_name("tm",tm).
-atom_name("yb",yb).
-atom_name("lu",lu).
-atom_name("hf",hf).
-atom_name("ta",ta).
-atom_name("w",w).
-atom_name("re",re).
-atom_name("os",os).
-atom_name("ir",ir).
-atom_name("pt",pt).
-atom_name("au",au).
-atom_name("hg",hg).
-atom_name("tl",tl).
-atom_name("pb",pb).
-atom_name("bi",bi).
-atom_name("po",po).
-atom_name("at",at).
-atom_name("rn",rn).
-atom_name("fr",fr).
-atom_name("ra",ra).
-atom_name("ac",ac).
-atom_name("th",th).
-atom_name("pa",pa).
-atom_name("u",u).
-atom_name("np",np).
-atom_name("pu",pu).
-atom_name("am",am).
-atom_name("cm",cm).
-atom_name("bk",bk).
-atom_name("cf",cf).
-atom_name("es",es).
-atom_name("fm",fm).
-atom_name("md",md).
-atom_name("no",no).
-atom_name("lr",lr).
-atom_name("rf",rf).
-atom_name("db",db).
-atom_name("sg",sg).
-atom_name("bh",bh).
-atom_name("hs",hs).
-atom_name("mt",mt).
-atom_name("ds",ds).
-atom_name("rg",rg).
-atom_name("cn",cn).
-atom_name("uut",uut).
-atom_name("fl",fl).
-atom_name("uup",uup).
-atom_name("lv",lv).
+atom_name(" h ", h , hydrogen).
+atom_name(" he ", he , helium).
+atom_name(" li ", li , lithium).
+atom_name(" be ", be , beryllium).
+atom_name(" b ", b , boron).
+atom_name(" c ", c , carbon).
+atom_name(" n ", n , nitrogen).
+atom_name(" o ", o , oxygen).
+atom_name(" f ", f , fluorine).
+atom_name(" ne ", ne , neon).
+atom_name(" na ", na , sodium).
+atom_name(" mg ", mg , magnesium).
+atom_name(" al ", al , aluminum or aluminium).
+atom_name(" si ", si , silicon).
+atom_name(" p ", p , phosphorus).
+atom_name(" s ", s , sulfur).
+atom_name(" cl ", cl , chlorine).
+atom_name(" ar ", ar , argon).
+atom_name(" k ", k , potassium).
+atom_name(" ca ", ca , calcium).
+atom_name(" sc ", sc , scandium).
+atom_name(" ti ", ti , titanium).
+atom_name(" v ", v , vanadium).
+atom_name(" cr ", cr , chromium).
+atom_name(" mn ", mn , manganese).
+atom_name(" fe ", fe , iron).
+atom_name(" co ", co , cobalt).
+atom_name(" ni ", ni , nickel).
+atom_name(" cu ", cu , copper).
+atom_name(" zn ", zn , zinc).
+atom_name(" ga ", ga , gallium).
+atom_name(" ge ", ge , germanium).
+atom_name(" as ", as , arsenic).
+atom_name(" se ", se , selenium).
+atom_name(" br ", br , bromine).
+atom_name(" kr ", kr , krypton).
+atom_name(" rb ", rb , rubidium).
+atom_name(" sr ", sr , strontium).
+atom_name(" y ", y , yttrium).
+atom_name(" zr ", zr , zirconium).
+atom_name(" nb ", nb , niobium).
+atom_name(" mo ", mo , molybdenum).
+atom_name(" tc ", tc , technetium).
+atom_name(" ru ", ru , ruthenium).
+atom_name(" rh ", rh , rhodium).
+atom_name(" pd ", pd , palladium).
+atom_name(" ag ", ag , silver).
+atom_name(" cd ", cd , cadmium).
+atom_name(" in ", in , indium).
+atom_name(" sn ", sn , tin).
+atom_name(" sb ", sb , antimony).
+atom_name(" te ", te , tellurium).
+atom_name(" i ", i , iodine).
+atom_name(" xe ", xe , xenon).
+atom_name(" cs ", cs , cesium).
+atom_name(" ba ", ba , barium).
+atom_name(" la ", la , lanthanum).
+atom_name(" ce ", ce , cerium).
+atom_name(" pr ", pr , praseodymium).
+atom_name(" nd ", nd , neodymium).
+atom_name(" pm ", pm , promethium).
+atom_name(" sm ", sm , samarium).
+atom_name(" eu ", eu , europium).
+atom_name(" gd ", gd , gadolinium).
+atom_name(" tb ", tb , terbium).
+atom_name(" dy ", dy , dysprosium).
+atom_name(" ho ", ho , holmium).
+atom_name(" er ", er , erbium).
+atom_name(" tm ", tm , thulium).
+atom_name(" yb ", yb , ytterbium).
+atom_name(" lu ", lu , lutetium).
+atom_name(" hf ", hf , hafnium).
+atom_name(" ta ", ta , tantalum).
+atom_name(" w ", w , tungsten).
+atom_name(" re ", re , rhenium).
+atom_name(" os ", os , osmium).
+atom_name(" ir ", ir , iridium).
+atom_name(" pt ", pt , platinum).
+atom_name(" au ", au , gold).
+atom_name(" hg ", hg , mercury).
+atom_name(" tl ", tl , thallium).
+atom_name(" pb ", pb , lead).
+atom_name(" bi ", bi , bismuth).
+atom_name(" po ", po , polonium).
+atom_name(" at ", at , astatine).
+atom_name(" rn ", rn , radon).
+atom_name(" fr ", fr , francium).
+atom_name(" ra ", ra , radium).
+atom_name(" ac ", ac , actinium).
+atom_name(" th ", th , thorium).
+atom_name(" pa ", pa , protactinium).
+atom_name(" u ", u , uranium).
+atom_name(" np ", np , neptunium).
+atom_name(" pu ", pu , plutonium).
+atom_name(" am ", am , americium).
+atom_name(" cm ", cm , curium).
+atom_name(" bk ", bk , berkelium).
+atom_name(" cf ", cf , californium).
+atom_name(" es ", es , einsteinium).
+atom_name(" fm ", fm , fermium).
+atom_name(" md ", md , mendelevium).
+atom_name(" no ", no , nobelium).
+atom_name(" lr ", lr , lawrencium).
+atom_name(" rf ", rf , rutherfordium).
+atom_name(" db ", db , dubnium).
+atom_name(" sg ", sg , seaborgium).
+atom_name(" bh ", bh , bohrium).
+atom_name(" hs ", hs , hassium).
+atom_name(" mt ", mt , meitnerium).
+atom_name(" ds ", ds , darmstadtium).
+atom_name(" rg ", rg , roentgenium).
+atom_name(" cn ", cn , copernicium).
+atom_name(" uut ", uut , ununtrium).
+atom_name(" uuq ", uuq , ununquadium).
+atom_name(" uup ", uup , ununpentium).
+atom_name(" uuh ", uuh , ununhexium).
+atom_name(" uus ", uus , ununseptium).
+atom_name(" uuo ", uuo , ununoctium ).
+
+
 
 molecule(A,[An]):-atom_name(A,An).
 molecule(M,[An|T]):-atom_name(A,An),append(A,M2,M),molecule(M2,T).
